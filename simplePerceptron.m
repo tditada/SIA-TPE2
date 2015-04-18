1af99c36bd59c5dd743602b0540855f0045c9ba8
Björn Mattsson <bamattsson@gmail.com>
2015-04-18 18:20:25 -0300

    Modifying simplePerceptron.m

--- simplePerceptron.m
+++ simplePerceptron.m
@@ -1,22 +1,18 @@
 function ret=simplePerceptron(n)
 
 disp('Entrando al perceptron simple');
-umbral=0.5;
-tasa= 0.05;
-weight=zeros(1,n); %hacer aleatorio
+tasa= 0.5;
+weight=zeros(1,n + 1);
+for i = 1:n + 1
+    weight(i) = 2*rand() - 1;
+end
 [training, V] = generateTrainingAnd(n);
-
+training = [-1 * ones(size(training,1),1) training];
 while 1
     count = 0;
-%     for j=1:n
-%         disp(weight(j));
-%     end
     for i=1:size(training,1)
         sum= training(i,:)*weight';
-%         disp('sum y V')
-%         disp(sum)
-%         disp(V(i))
-        if (sum > umbral)
+        if (sum > 0)
             ret=1;
         else
             ret=0;
@@ -24,13 +20,18 @@ while 1
         error = V(i)-ret;
         if(error~=0)
             count = count + 1;
-            for z=1:n
+            for z=1:n + 1
                 weight(z) = weight(z)+ tasa*error*training(i,z);
             end
         end
     end
     if (count == 0)
         break;
+    end
+    if (tasa > 0.05)
+        tasa = tasa - 0.05;
+    elseif (tasa < 0.05)
+        tasa = 0.01;
     end
 end
 
