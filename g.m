db1ba33c58b16f9ad33edb5e6127ad32da3d7b30
Björn Mattsson <bamattsson@gmail.com>
2015-04-21 18:27:35 -0300

    Adding small correction to 2 functions

--- g.m
+++ g.m
@@ -2,12 +2,9 @@ function o = g(gName, x, threshold)
 	switch gName
 		case 'step'
 			o = stepFunction(x, threshold);
-			break;
 		case 'sigmodea'
 			o = tanh(0.5*x);
-			break;
 		case 'lineal'
-			break;
 		otherwise
 			disp('not a valid function');
 	end
--- gDerivated.m
+++ gDerivated.m
@@ -2,7 +2,6 @@ function o = gDerivated(gName, x)
 	switch gName
 		case 'sigmodea'
 			o = 0.5*(1-(tanh(0.5*x)^2));
-			break;
 		otherwise
 			disp('not a valid function');
 	end
