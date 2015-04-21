5f37ebe379ea5d9422e4e9ee3411a54a90b76c27
Björn Mattsson <bamattsson@gmail.com>
2015-04-21 16:19:22 -0300

    Name and small changes in twoLayerGenerator.m -> weightGenerator.m

---
+++ weightGenerator.m
@@ -0,0 +1,13 @@
+function  weights = weightGenerator( inputAmount, outputAmount )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+weights = zeros(outputAmount, inputAmount + 1);
+
+for i = 1:outputAmount
+    for j = 1:inputAmount + 1
+        weights(i,j) = rand*2 - 1;
+    end
+end
+
+end
