e4d5bc48edccacf93bbc85f99f4e2a861ee07b47
Björn Mattsson <bamattsson@gmail.com>
2015-04-21 21:12:08 -0300

    Adding function that aggregates all added changes

--- generateTraining.m
+++ generateTraining.m
@@ -2,16 +2,12 @@ function [training, expected] = generateTraining(learningFunction, inputAmount)
 	switch learningFunction
 		case 'and'
 			[training, expected]=generateTrainingAnd(inputAmount);
-			break;
 		case 'or'
 			[training, expected]=generateTrainingOr(inputAmount);
-			break;
 		case 'parity'
 			[training, expected]=generateTrainingParity(inputAmount);	
-			break;
 		case 'simetry'
 			[training, expected]=generateTrainingSimetry(inputAmount);
-			break;
 		otherwise
 			disp('malisimo');
 	end
--- multiLayeredPerceptron.m
+++ multiLayeredPerceptron.m
@@ -1,39 +1,58 @@
-function ret = multiLayeredPerceptron(inputAmount, middleAmount, outputAmount, learningFunction, gName)
-	%learningRate = 0.1;
-	[training, expected] = generateTraining(learningFunction, inputAmount);
-	firstLayerWeights = twoLayerGenerator(inputAmount, middleAmount);
-	secondLayerWeights = twoLayerGenerator(middleAmount, outputAmount);
-	firstOutput = layerOutput(training, firstLayerWeights, gName, middleAmount);
-	secondOutput = layerOutput(firstOutput, secondLayerWeights, gName, outputAmount);
-	
-	disp('first layer weights');
-	disp(firstLayerWeights);
-	disp('second layer weights');
-	disp(secondLayerWeights);
-	disp('training');
-	disp(training);
-	disp('first output');
-	disp(firstOutput);
-	ret = secondOutput;
-	disp('ret');
-	disp(ret);
+function ret = multiLayeredPerceptron(inputAmount, middleAmount, learningFunction, gName)
+%learningRate = 0.1;
+[training, expected] = generateTraining(learningFunction, inputAmount);
+W_1 = weightGenerator(inputAmount, middleAmount);
+W_2 = weightGenerator(middleAmount, 1);
 
-	%TO-DO: backpropagation
-	%vec_error = expected - ret;
-	%deriv = zeros(size(training,1), 1);
-	%for i = 1:size(training,1)
-	%	deriv(i,1) = gDerivated(gName, h2(1,1));
-	%end
+training = [-1*ones(size(training,1),1) training];
+
+change_weight = 0.3;
+
+diff = zeros(1000,1);
+
+for i = 1:1000
+    fL = zeros(size(W_1));
+    sL = zeros(size(W_2));
+    E = 0;
+    for training_number = 1:size(training,1)
+        [h_1, V] = calculateLayer(W_1, transpose(training(training_number,:)), gName);
+        [h_2, o] = calculateLayer(W_2, V, gName);
+        E = E + (expected(training_number) - o(2))^2;
+        
+        
+        [fLd, sLd] = changeMatrix(transpose(training(training_number,:)), h_1, V, W_2, h_2, o, expected(training_number), gName);
+        fL = fL + fLd;
+        sL = sL + sLd;
+    end
+    
+    W_1 = W_1 + change_weight*fL;
+    W_2 = W_2 + change_weight*sL;
+    if (change_weight > 0.05)
+        change_weight = change_weight*0.95;
+    elseif (change_weight <= 0.001)
+        change_weight = 0.001;
+    end
+    diff(i) = 1/2*E;
+end
+
+plot(diff); shg;
+
+%TO-DO: backpropagation
+%vec_error = expected - ret;
+%deriv = zeros(size(training,1), 1);
+%for i = 1:size(training,1)
+%	deriv(i,1) = gDerivated(gName, h2(1,1));
+%end
 end
 
 function ans = layerOutput(training, firstLayerWeights, gName, outputSize)
-	ans = zeros(size(training,1), outputSize);
-	for i = 1:size(training,1)
-		givenInput = transpose(training(i,:));
-		aux = preOutput(givenInput, firstLayerWeights);
-		for j = 1: size(aux,1);
-			o = g(gName, aux(j,1), firstLayerWeights(j,1));
-			ans(i, j) = o;
-		end
-	end
+ans = zeros(size(training,1), outputSize);
+for i = 1:size(training,1)
+    givenInput = transpose(training(i,:));
+    aux = preOutput(givenInput, firstLayerWeights);
+    for j = 1: size(aux,1);
+        o = g(gName, aux(j,1), firstLayerWeights(j,1));
+        ans(i, j) = o;
+    end
+end
 end
