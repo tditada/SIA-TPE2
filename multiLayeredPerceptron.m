952fafdbb04ceaa1c0897aa50a1def1ec77d973d
Björn Mattsson <bamattsson@gmail.com>
2015-04-29 21:40:46 -0300

    Adding changes. Now sucessfully imitates some of the functions features.

--- multiLayeredPerceptron.m
+++ multiLayeredPerceptron.m
@@ -1,63 +1,94 @@
-function [W_1_best, W_2_best, diff, w_v] = multiLayeredPerceptron(trainingSet, middleAmount, gName, maxIt, calcAllFreq, ETol)
+function [W_1_best, W_2_best, diff] = multiLayeredPerceptron(trainingSet, middleAmount, gName, maxIt, calcAllFreq, ETol)
 
 training = trainingSet(:,1:end-1);
 expected = trainingSet(:,end);
 inputAmount = size(training,2);
 
+% Generate random weights.
 W_1 = weightGenerator(inputAmount, middleAmount);
 W_2 = weightGenerator(middleAmount, 1);
+W_1_best = W_1; W_2_best = W_2;
 
-E_best = -1;
-
+% Adding the first column of -1 to training-set.
 training = [-1*ones(size(training,1),1) training];
 
-change_weight = 0.01;
+% Start values
+change_weight = 0.001;
+E_best = -1;
+decreaseCounter = 0;
 
 for i = 1:maxIt
     fL = zeros(size(W_1));
     sL = zeros(size(W_2));
     E = 0;
-    if (mod(i,calcAllFreq) == 0)
+    if (mod(i,calcAllFreq) == 0 || i == 1)
+        %To calculate how the error changes.
         for training_number = 1:size(training,1)
             [h_1, V] = calculateLayer(W_1, transpose(training(training_number,:)), gName);
             [h_2, o] = calculateLayer(W_2, V, 'lineal');
-            E = E + (expected(training_number) - o(2))^2;
-            
-%            [fLd, sLd] = changeMatrix(transpose(training(training_number,:)), h_1, V, W_2, h_2, o, expected(training_number), gName);
-%            fL = fL + fLd;
-%            sL = sL + sLd;
+            E = E + 1/2*(expected(training_number) - o(2))^2;
         end
-        if (E > E_best && E_best ~= -1)
-            fL = 0; sL = 0;
+
+        % Part that regulates how change_weight changes. Decrease if the
+        % error doesn't decrease. Increase if error decreases many times in
+        % a row.
+        if (E >= E_best && E_best ~= -1)
             W_1 = W_1_best;
             W_2 = W_2_best;
-        else 
+            change_weight = 0.9*change_weight;
+            decreaseCounter = 0;
+            if (change_weight < 10^-10)
+                change_weight = 0.1;
+            end
+        elseif (E < E_best || E_best == -1)
             E_best = E;
             W_1_best = W_1;
             W_2_best = W_2;
+            decreaseCounter = decreaseCounter + 1;
+            if (decreaseCounter == 5)
+                change_weight = 2*change_weight;
+                decreaseCounter = 0;
+            end
+        end
+        % Adding a new data point over error-history.
+        if(i ~= 1)
+            diff(i/calcAllFreq) = E;
         end
         
-        diff(i/calcAllFreq) = 1/2*E;
-        w_v(i/calcAllFreq) = change_weight;
+        % Output. Both to command-line and to screen.
+        if (mod(i,20*calcAllFreq) == 0)
+            disp(W_1_best);
+            disp(W_2_best);
+            disp(i/maxIt);
+            disp(E_best);
+            Out = zeros(size(training,1),1);
+            for j=1:size(training,1)
+                [h_1, V] = calculateLayer(W_1_best, transpose(training(j,:)), gName);
+                [h_2, o] = calculateLayer(W_2_best, V, 'lineal');
+                Out(j) = o(2);
+            end
+            subplot(1,2,1); plot(diff(20:end));
+            subplot(1,2,2);
+            plot(training(:,2)',Out); hold on;
+            plot(training(:,2)',expected,'r*'); hold off; shg;
+        end
+        
+        % Break if error is smaller than tollerance
         if (E < ETol)
             break;
         end
     else
+        % To calculate how we should change the weights and changing the weights.
         training_number = floor(rand()*size(training,1)+1);
         [h_1, V] = calculateLayer(W_1, transpose(training(training_number,:)), gName);
         [h_2, o] = calculateLayer(W_2, V, 'lineal');
         
         [fL, sL] = changeMatrix(transpose(training(training_number,:)), h_1, V, W_2, h_2, o, expected(training_number), gName);
+        
+        W_1 = W_1 + change_weight*fL;
+        W_2 = W_2 + change_weight*sL;
     end
-    
-    W_1 = W_1 + change_weight*fL;
-    W_2 = W_2 + change_weight*sL;
 
-    if (change_weight > 0.00001)
-        change_weight = change_weight*0.99;
-    else
-        change_weight = 0.0001;
-    end
 end
 
 end
--- test.m
+++ test.m
@@ -1,27 +1,26 @@
 %% Test
 clear; clf
-trainingAmount = 100;
-middleAmount = 30;
-maxIt = 50000;
-calcAllFreq = 100;
+trainingAmount = 200;
+middleAmount = 15;
+maxIt = 10000000;
+calcAllFreq = trainingAmount;
 ETol = 0.005;
 gName = 'sigmodea';
 [training, expected] = generateTrainingTPfunction(trainingAmount,0);
 training = [training, expected];
 
-[W_1, W_2, diff, w_v] = multiLayeredPerceptron(training, middleAmount, gName, maxIt, calcAllFreq, ETol);
-subplot(2,2,1);
-plot(diff(10:end)); shg;
-subplot(2,2,3);
-semilogy(w_v(10:end))
+[W_1, W_2, diff] = multiLayeredPerceptron(training, middleAmount, gName, maxIt, calcAllFreq, ETol);
 
-%
+%%
+subplot(1,2,1);
+plot(diff); shg;
+
 Out = zeros(trainingAmount,1);
 for i=1:trainingAmount
     [h_1, V] = calculateLayer(W_1, transpose(training(i)), gName);
     [h_2, o] = calculateLayer(W_2, V, 'lineal');
     Out(i) = o(2);
 end
-subplot(2,2,2);
+subplot(1,2,2);
 plot(training(:,1),Out); hold on;
 plot(training(:,1),expected,'r*'); shg
