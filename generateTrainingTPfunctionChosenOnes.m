dd037ecb98ef66df479f2bb5ee4f4ac3b301fd73
Teresa <tditada@gmail.com>
2015-05-02 18:27:54 -0300

    change in eta for one layer with saturation control

--- generateTrainingTPfunctionChosenOnes.m
+++ generateTrainingTPfunctionChosenOnes.m
@@ -1,8 +1,8 @@
 function  [ matrix, V ]  = generateTrainingTPfunctionChosenOnes(amount)
 	X = [linspace(-1,-0.95,amount/6), linspace(-0.9,-0.6,amount/6), linspace(-0.5,-0.155,2*(amount/6)), linspace(0.1,0.5,amount/6), linspace(0.8,1,amount/6)];
-	aux = randperm(amount);
-	matrix = zeros(1, amount);
-	for i = 1:amount
+	aux = randperm(size(X,2));
+	matrix = zeros(1, size(X,2));
+	for i = 1:size(X,2)
 		matrix(i) = X(aux(i));
 	end
 	matrix = transpose(X);
--- test.m
+++ test.m
@@ -9,25 +9,8 @@ gName = 'sigmodea';
 [training, expected] = generateTrainingTPfunctionChosenOnes(trainingAmount);
 training = [training, expected];
 
-<<<<<<< HEAD
+
 [W_1, W_2, diff] = multiLayeredPerceptronWithSaturation(training, middleAmount, gName, maxIt, calcAllFreq, ETol);
-
-%%
-%subplot(1,2,1);
-%plot(diff); shg;
-
-%Out = zeros(trainingAmount,1);
-%for i=1:trainingAmount
-%    [h_1, V_1] = calculateLayer(W_1, transpose(training(training_number,:)), gName);
-%    [h_2, V_2] = calculateLayer(W_2, V_1, gName);
-%    [h_3, o] = calculateLayer(W_3, V_2, 'lineal');
-%    Out(j) = o(2);
-%end
-%subplot(1,2,2);
-%plot(training(:,1),Out); hold on;
-%plot(training(:,1),expected,'r*'); shg
-=======
-[W_1, W_2, diff] = multiLayeredPerceptron(training, middleAmount, gName, maxIt, calcAllFreq, ETol);
 subplot(2,1,1)
 plot(diff); shg;
 
@@ -42,4 +25,3 @@ end
 subplot(2,1,2)
 plot(training(:,1),Out); hold on;
 plot(training(:,1),expected,'r'); shg
->>>>>>> 8c75712b3bf0a11f880831e7a7029b2f545108d7
