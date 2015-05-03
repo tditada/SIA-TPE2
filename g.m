0a9f3566feb725d4d239a2064ecb89d40f72cf62
valeserber <valserber@gmail.com>
2015-05-03 18:04:15 -0300

    added exponencial

--- g.m
+++ g.m
@@ -2,8 +2,10 @@ function o = g(gName, x, threshold)
 	switch gName
 		case 'step'
 			o = stepFunction(x, threshold);
-		case 'sigmodea'
+		case 'tangente'
 			o = tanh(0.5*x);
+		case 'exponencial'
+			o = 1/(1+e^(-2*0.5*x));
 		case 'lineal'
 			o = x;
 		otherwise
