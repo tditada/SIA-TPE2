4012878300756caa450e3fc4d84d15a700b6ad77
valeserber <valserber@gmail.com>
2015-05-04 16:30:51 -0300

    adsdas

--- test2.m
+++ test2.m
@@ -6,7 +6,7 @@ middleAmount{2} = 3;
 maxIt = 10000000;
 ETol = 0.25;
 saturationControl = 25;
-gName = 'exponencial';
+gName = 'tangente';
 [training, expected] = generateTrainingTPfunctionChosenOnes(trainingAmount);
 training = [training, expected];
 
@@ -18,6 +18,6 @@ w_3 = csvread('W_3_neurons_3_1.csv');
 %function [W, diff] = trainNet(trainingAmount, hiddenAmount, middleAmount, maxIt, Etol, gName, saturationControl)
 %[W, diff] = multiLayeredPerceptron2(training, middleAmount, gName, maxIt, ETol);
 %function [W, diff] = trainNet(trainingAmount, hiddenAmount, middleAmount, maxIt, Etol, gName, saturationControl, etha adaptativo)
-[W, diff] = trainNet(w_1, w_2, w_3, trainingAmount, 2, middleAmount,maxIt, ETol, gName, 25, 1);
+[W, diff] = trainNet(w_1, w_2, w_3, trainingAmount, 2, middleAmount,maxIt, ETol, gName, -1, 1);
 %[W, diff] = trainNet();
 % testNet()
